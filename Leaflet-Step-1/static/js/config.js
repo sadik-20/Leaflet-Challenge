45a0dabac0fd5c410b915510c353d37144a7af27
smuktar <limodirectphilly@gmail.com>
2021-01-29 18:56:47 -0500

    add tile layer with north america centered map

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoic2FkaWttYXBib3giLCJhIjoiY2tqendiMGU1MDY5ZjJ1cGJmazVqYnMzMyJ9.nTiMZO3esVyEohafFjDaSA";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,16 @@
+// Adding tile layer with variable
+var nAmerica = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+});
+// create Leaflet myMap var with center in North America
+var myMap = L.map("map", {
+  center: [39.14, -94.67],
+  zoom: 5
+});
+
+nAmerica.addTo(myMap);
